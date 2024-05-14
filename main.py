4277d4a14dace41d094d54b1d2b873b1255092a5
rajeshwari kiwad <8535931-rajeshwarikiwad@users.noreply.replit.com>
2024-05-14 17:10:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("enter the number1")
+a=int(input())
+print("enter the operation")
+c=input()
+print("enter the number2")
+b=int(input())
+if(c=='+'):
+  print(a+b)
+elif(c=='-'):
+  print(a-b)
+elif(c=='*'):
+  print(a*b)
+elif(c=='/'):
+  print(a/b)
+  
